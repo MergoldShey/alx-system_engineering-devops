f45904c4295573834f561348d010a356dfce5744
MergoldShey <soronnadishiphiah@gmail.com>
2023-01-12 16:15:34 +0000

    creating a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates a file in /tmp
 
 file {'/tmp/school':
-    mode => '0744',
     content => 'I love puppet',
+    mode => '0744',
     owner => 'www-data',
-    group => 'www-data',}
+    group => 'www-data',
+}
